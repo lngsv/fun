64e0b3f6530791e8aaf45f7c659cc107577ff72e
Alan Gasiev <alan_gassiev@mail.ru>
2021-03-29 12:26:06 +0300

    generalize generating xls

--- generate_xls.go
+++ generate_xls.go
@@ -3,13 +3,23 @@ package main
 import (
 	"fmt"
 	"github.com/360EntSecGroup-Skylar/excelize"
+    "strconv"
 )
 
 func main() {
 	f := excelize.NewFile()
+	f.AddTable("Sheet1", "A1", "B1", ``)
+	style, err := f.NewStyle(`{"font": {"bold":true}}`)
+	if err != nil {
+		fmt.Println("error new style", err)
+	}
 	f.SetSheetRow("Sheet1", "A1", &[]string{"col1", "col2"})
-	f.SetSheetRow("Sheet1", "A2", &[]interface{}{"15", 123.5})
-	err := f.SaveAs("mytable.xls")
+	f.SetCellStyle("Sheet1", "A1", "B1", style)
+    rows := [][]interface{}{{"15", 123.5}, {true, 55}}
+    for i, row := range rows {
+		f.SetSheetRow("Sheet1", "A"+strconv.Itoa(i+2), &row)
+	}
+	err = f.SaveAs("mytable.xls")
 	if err != nil {
 		fmt.Println("cannot save", err)
 	}
