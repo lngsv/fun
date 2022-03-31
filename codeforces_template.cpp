ddb447c6cf98b847ec11045d94b3760346efb4e9
Alan Gasiev <alan_gassiev@mail.ru>
2022-03-31 20:00:19 +0300

    add codeforces template

---
+++ codeforces_template.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <sstream>
+#include <algorithm>
+#include <cstdio>
+#include <cstdlib>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+
+void solve() {
+
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+}
+
